291749d8f3f6ff3b2563a3f18cb545f7033ca041
ivana-queiroz-assis <ivanaqueirozassis@hotmail.com>
2018-03-10 23:48:25 -0300

    testando heroku

--- evento/src/main/java/br/com/ivana/evento/DataConfiguration.java
+++ evento/src/main/java/br/com/ivana/evento/DataConfiguration.java
@@ -1,5 +1,5 @@
 package br.com.ivana.evento;
-import java.net.URI;
+
 import java.net.URISyntaxException;
 
 import org.apache.commons.dbcp2.BasicDataSource;
@@ -16,16 +16,15 @@ public class DataConfiguration{
 
 	@Bean
     public BasicDataSource dataSource() throws URISyntaxException {
-        URI dbUri = new URI(System.getenv("DATABASE_URL"));
+       
 
-        String username = dbUri.getUserInfo().split(":")[0];
-        String password = dbUri.getUserInfo().split(":")[1];
-        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+        
+        String dbUrl = "jdbc:postgresql://" + "ec2-107-22-168-211.compute-1.amazonaws.com" + ':' + "5432"+ "d6pemk2qv7060u";
 
         BasicDataSource basicDataSource = new BasicDataSource();
         basicDataSource.setUrl(dbUrl);
-        basicDataSource.setUsername(username);
-        basicDataSource.setPassword(password);
+        basicDataSource.setUsername("ikrhhgqlyjmxda");
+        basicDataSource.setPassword("34574e17fe2320bc5110f109a4db908594deff548179e6c1ef553c9d92a9dbe0");
 
         return basicDataSource;
     }
